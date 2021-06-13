1b86a624c8993bba246b2f08f32f90676cd5c738
sam <wax654@gmail.com>
2021-06-13 13:23:50 -0700

    Added first component

---
+++ part-1/FirstComponent.js
@@ -0,0 +1,6 @@
+// FirstComponent
+// renders an h1 with the text “My very first component”.
+
+const FirstComponent = () => (
+    <h1>My very first component</h1>
+);
--- part-1/index.js
+++ part-1/index.js
@@ -1,8 +1,3 @@
-// FirstComponent
-// renders an h1 with the text “My very first component”.
-
-// NamedComponent
-// renders a p that should accept a property of “name” and display text “My name is name”.
 
 // App
 // renders a div with instances of the other two components.
