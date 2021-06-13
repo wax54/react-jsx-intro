290edd54fd2723d3976d35e8f8c49cc80b450116
sam <wax654@gmail.com>
2021-06-13 13:25:56 -0700

    added named component

---
+++ part-1/NamedComponent.js
@@ -0,0 +1,7 @@
+
+// NamedComponent
+// renders a p that should accept a property of “name” and display text “My name is name”.
+
+const NamedComponent = ({name}) => (
+    <p>My name is {name}</p>
+);
